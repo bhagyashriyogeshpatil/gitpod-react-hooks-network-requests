bd27b6d15063e7148f16857c952198596ed4bcd2
Bhagyashri Yogesh Patil <bhagyashriyogeshpatil@gmail.com>
2024-08-30 21:23:52 +0000

    Added UseStateWithArrays component to demonstrate useState() with arrays, including add, remove, and buggy push functionality.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import HooksCounter from "./components/HooksCounter";
 import ClassCounter from "./components/ClassCounter";
 import ControlledFormHooks from "./components/ControlledFormHooks";
+import UseStateWithArrays from "./components/UseStateWithArrays";
 
 function App() {
   return <div className="App">
       {/* <HooksCounter /> */}
       {/* <ClassCounter /> */}
-      <ControlledFormHooks />
+      {/* <ControlledFormHooks /> */}
+      <UseStateWithArrays /> 
     </div>;
 }
 
