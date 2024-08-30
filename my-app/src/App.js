a8129a789876cda1001ebc074cd9ffb9c95e5c06
Bhagyashri Yogesh Patil <bhagyashriyogeshpatil@gmail.com>
2024-08-30 20:35:32 +0000

    Initial commit: Added ClassCounter (stateful class component) and HooksCounter (functional component with hooks)

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import HooksCounter from "./components/HooksCounter";
+import ClassCounter from "./components/ClassCounter";
+
+function App() {
+  return <div className="App">
+      <HooksCounter />
+      <ClassCounter />
+    </div>;
+}
+
+export default App;
