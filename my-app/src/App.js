7d40fc3b9fc2c8d04b5522b4afdd2c2ca3033058
Bhagyashri Yogesh Patil <bhagyashriyogeshpatil@gmail.com>
2024-08-31 12:57:44 +0000

    add UseStateWithObjects component to manage form input with useState and object

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,13 +3,15 @@ import HooksCounter from "./components/HooksCounter";
 import ClassCounter from "./components/ClassCounter";
 import ControlledFormHooks from "./components/ControlledFormHooks";
 import UseStateWithArrays from "./components/UseStateWithArrays";
+import UseStateWithObjects from "./components/UseStateWithObjects";
 
 function App() {
   return <div className="App">
       {/* <HooksCounter /> */}
       {/* <ClassCounter /> */}
       {/* <ControlledFormHooks /> */}
-      <UseStateWithArrays /> 
+      {/* <UseStateWithArrays />  */}
+      <UseStateWithObjects />
     </div>;
 }
 
